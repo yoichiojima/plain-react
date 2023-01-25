853d6b7c91c961c6fcefef965cfbf5ea586b6f65
Yoichi Ojima <yoichiojima@gmail.com>
2023-01-25 20:45:31 +0900

    install chakra

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+import { ChakraProvider, Box } from "@chakra-ui/react"
 import './App.css';
 
 const App = () => {
   return (
-    <></>
+    <ChakraProvider>
+      <Box className="App">
+      </Box>
+    </ChakraProvider>
   );
 }
 
